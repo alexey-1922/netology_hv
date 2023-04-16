fe8148139c0bb07cb59785fec03b4691ed52ce01
Alexey Alyabiev <alexalabev@yandex.ru>
2023-04-16 23:27:06 +0300

    first commit

---
+++ hv_open_read_file.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+with open('recipes.txt', encoding = 'utf-8') as file:
+    cook_book = {}
+    for line in file:
+        dish_name = line.strip()
+        ingredients_count = int(file.readline().strip())
+        ingredients = []
+        for _ in range(ingredients_count):
+            ingr, quant, meas = file.readline().strip().split(' | ')
+            ingredients.append({'ingredient_name': ingr, 'quantity': quant, 'measure': meas})
+        file.readline()
+        cook_book[dish_name] = ingredients
+    #pprint(cook_book, sort_dicts = False)      
+    #pprint(cook_book['Омлет'])      
+    def main(dishes, person_count):  
+        result = {}
+        for key in dishes:
+            for res_dict in cook_book[key]:
+                nested_dict = {}
+                if res_dict['ingredient_name'] not in result.keys():
+                    nested_dict['measure'] = res_dict['measure']
+                    nested_dict['quantity'] = int(res_dict['quantity']) * person_count
+                    result[res_dict['ingredient_name']] = nested_dict
+                else:
+                    nested_dict = result[res_dict['ingredient_name']]
+                    nested_dict['quantity'] += int(res_dict['quantity']) * person_count
+                           
+        pprint(result)                     
+if __name__ == '__main__':
+    main(['Фахитос', 'Омлет'], 2)
